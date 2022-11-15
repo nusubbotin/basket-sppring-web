84c9a5d274d5bd286d249c83941ae3fb8a80eb87
nusubbotin <nu.subbotin@ya.ru>
2022-11-15 15:48:38 +0300

    инициализация проекта под web-сервис Козина

---
+++ src/main/java/com/skypro/basket/BasketSppringWebApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.basket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketSppringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasketSppringWebApplication.class, args);
+	}
+
+}
